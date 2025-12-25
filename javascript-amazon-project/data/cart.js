95d3dcf2a304f9fd70843bd184cb62d747fc2a50
Der324 <ntalenderick@gmail.com>
2025-12-25 03:03:03 +0300

    Save data and generate HTML for delivery options

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,16 +1,19 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
-  cart = [
-    {
+  cart = [{
+
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
-}, 
-{
+  deliveryOptionid: '1'
+
+}, {
+
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}
-];
+  quantity: 1,
+  deliveryOptionid: '2'
+
+}];
 
 }
 
@@ -32,8 +35,9 @@ export function addToCart(productId) {
       matchingItem.quantity +=1;
     } else {
       cart.push({
-        productId,
-        quantity: 1
+        productId: productId,
+        quantity: 1,
+        deliveryOptionid: '1'
       });
     }
 
