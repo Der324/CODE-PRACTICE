970523d54f4338975fa468205bdde773109337cd
Der324 <ntalenderick@gmail.com>
2026-01-17 11:02:07 +0200

    Create class version of cart.

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderCheckoutHeader } from "./utils/CheckoutHeader.js";
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
+import '../data/cart-class.js';
 import '../data/cart-oop.js';
 
 renderCheckoutHeader();
